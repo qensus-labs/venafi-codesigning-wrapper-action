5265a4b62935162f2b42d787562b1856e3df4e57
avwsolutions <arnold.van.wijnbergen@gmail.com>
2023-09-27 16:24:44 +0200

    Polish

--- action.js
+++ action.js
@@ -53,7 +53,7 @@ async function downloadCSPDriver(currentOs, version) {
     }
 
     // Cache the downloaded tool so we do not have to download multiple times
-    cachedToolPath = await tc.cacheDir(downloadPath, toolName, version);
+    cachedToolPath = await tc.cacheFile(downloadPath, toolName, version);
   }
 
   // Get the full path to the executable
