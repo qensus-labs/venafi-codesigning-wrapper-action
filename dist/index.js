30a7586e6d6e56f8155a846b6cf274ee0d0288ab
avwsolutions <arnold.van.wijnbergen@gmail.com>
2024-07-13 14:24:27 +0200

    Added support for `24.1.0`

--- dist/index.js
+++ dist/index.js
@@ -202,7 +202,9 @@ function walkSync(dir, fileList, fileToFind) {
 // The main function of this action. After the archive is downloaded and
 // extracted this function adds it location to the path. This will make sure
 // other steps in your workflow will be able to call the CSP Driver.
-async function run(currentOs, version) {
+async function run(currentOs, currentDistro, version) {
+  core.info(`Identified '${currentDistro}' for ${currentOs}`);
+  
   let cachedPath = await downloadCSPDriver(baseURL, currentOs, version);
 
   if (!process.env["PATH"].startsWith(path.dirname(cachedPath))) {
@@ -6977,8 +6979,28 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
 const os = __nccwpck_require__(2037);
+const fs = __nccwpck_require__(7147);
 const action = __nccwpck_require__(1834);
 const core = __nccwpck_require__(6024);
+
+/*
+This function we determine the applicable distribution of the Linux operating system and sets the default distribution for other operating systems like Windows and MacOS.
+*/
+function getLinuxDistro(currentOs) {
+  if (currentOs == 'Linux') {
+    var distro; 
+    const data = fs.readFileSync('/etc/os-release', 'utf8');
+    const lines = data.toString().split('\n');
+    const idLine = lines.find(line => line.startsWith('ID='));
+    if (idLine) {
+          distro = idLine.split('=')[1];
+    }
+  }
+  else {
+    distro = 'default';
+  }
+    return distro;
+}
 
 /*
 This is how the action is used:
@@ -6998,8 +7020,14 @@ async function run() {
     // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     core.debug(new Date().toTimeString());
 
+    // determine current operating system used by github runner
+    const currentOs = os.type();
+
+    // determine current distro of the Linux operating system used by the github runner
+    const currentDistro = getLinuxDistro(currentOs);
+
     // run the action code
-    await action.run(os.type(), version);
+    await action.run(currentOs, currentDistro, version);
 
     core.info(new Date().toTimeString());
   } catch (error) {
